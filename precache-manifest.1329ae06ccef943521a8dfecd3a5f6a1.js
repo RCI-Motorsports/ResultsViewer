041b5c221d607cd301a2d38bee6cecc67e5d9712
Mikael Sundström <mikael@sundstrom.st>
2020-09-06 10:32:25 +0200

    Updates

---
+++ precache-manifest.1329ae06ccef943521a8dfecd3a5f6a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3a1be964af7b1148efbe524cd81f67",
+    "url": "/ResultsViewer/index.html"
+  },
+  {
+    "revision": "a8f4fe500fb869ded4b4",
+    "url": "/ResultsViewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb1eef2e05a40b445215",
+    "url": "/ResultsViewer/static/js/2.ca28a998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ResultsViewer/static/js/2.ca28a998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f4fe500fb869ded4b4",
+    "url": "/ResultsViewer/static/js/main.139c0a2e.chunk.js"
+  },
+  {
+    "revision": "c22b090b8fda564d9db5",
+    "url": "/ResultsViewer/static/js/runtime-main.bdd4f4ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ResultsViewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
